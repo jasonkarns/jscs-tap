2ce7ee04577361042a45b58aee58fcb4343bb138
Jason Karns <jason@karns.name>
2015-04-14 19:12:04 -0400

    tap output for jscs

--- index.js
+++ index.js
@@ -1,17 +1,69 @@
-module.exports = function reporter(results) {
-    var len = results.length,
-        str = '1..'+(len+1)+'\nok 1 Test was run\n',
-        file, error, counter = 1;
-
-    results.forEach(function (result) {
-        counter+=1;
-        file = result.file;
-        error = result.error;
-        str += 'not ok ' + counter + ' ' + file + ': line ' + error.line + ', col ' +
-            error.character + ', ' + error.reason + '\n';
-    });
-
-    if (str) {
-        process.stdout.write(str + "#TAP meta information\n" + len + ' errors\n');
-    }
+module.exports = function reporter(errorSets) {
+  var testGroups = errorSets.map(TestGroup.fromErrorSet);
+  process.stdout.write( testPlan(testGroups) );
+  process.stdout.write( testGroups.map(display).join("") );
 };
+
+function TestGroup(filename, failures){
+  this.filename = filename;
+  this.failures = failures;
+  this.testCount = failures.length || 1;
+  this.lintFree = !failures.length;
+}
+TestGroup.fromErrorSet = function(errorSet){
+  return new TestGroup(errorSet.getFilename(), errorSet.getErrorList().map(TestFailure.fromError));
+};
+TestGroup.prototype.display = function(template){
+  return template.displayTestGroup(this);
+};
+TestGroup.prototype.displayTests = function(template){
+  if(this.lintFree) {
+    return template.displayPassed(this.filename);
+  } else {
+    return this.failures.map(display).join("");
+  }
+};
+
+function TestFailure(filename, line, column, message){
+  this.filename = filename;
+  this.line = line;
+  this.column = column;
+  this.message = message;
+}
+TestFailure.fromError = function(error){
+  return new TestFailure(error.filename, error.line, error.column, error.message);
+};
+TestFailure.prototype.display = function(template){
+  return template.displayTestFailure(this);
+};
+
+var TapTemplate = {
+  displayTestGroup: function(testGroup){
+    return this.displayHeader(testGroup.filename) + testGroup.displayTests(this);
+  },
+  displayHeader: function(filename){
+    return "# " + filename + "\n";
+  },
+  displayPassed: function(description){
+    return "ok - " + description + "\n";
+  },
+  displayTestFailure: function(failure){
+    return "not ok - " +
+      failure.filename + ": " +
+      "line " + failure.line + ", " +
+      "col " + failure.column + ", " +
+      failure.message + "\n";
+  }
+};
+
+function testPlan(testGroups){
+  var total = testGroups.reduce(function(tally, group){
+    return tally + group.testCount;
+  }, 0);
+
+  return "1.." + total + "\n";
+}
+
+function display(results){
+  return results.display(TapTemplate);
+}
